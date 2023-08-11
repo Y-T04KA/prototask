46bf8111abd74124d98e5d24b1a58094ed081cc8
yt04ka <ytka@vivaldi.net>
2023-08-11 15:02:22 +0300

    read works, write works but crashes

--- read-test.cpp
+++ read-test.cpp
@@ -1,3 +1,31 @@
-//
-// Created by Kirill Rozhenko on 10.08.2023.
-//
+#include <string>
+#include "protobuf/work.pb.h"
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    prototask::WrapperMessage wm;
+    std::fstream input("proto_all.pb",std::ios::in | std::ios::binary);
+    if (!wm.ParseFromIstream(&input)){
+        std::cerr<<"fail to read\n";
+        return -1;
+    }
+    if (wm.has_fast_response()){
+        const prototask::FastResponse& fr = wm.fast_response();
+        std::cout << fr.current_date_time() << std::endl;
+    }
+    if (wm.has_slow_response()){
+        const prototask::SlowResponse& sr = wm.slow_response();
+        std::cout << sr.connected_client_count() << std::endl;
+    }
+    if (wm.has_request_for_fast_response()){
+        std::cout << "fast response!\n";
+    }
+    if (wm.has_request_for_slow_response()){
+        const prototask::RequestForSlowResponse& rsr = wm.request_for_slow_response();
+        std::cout << rsr.time_in_seconds_to_sleep();
+    }
+    google::protobuf::ShutdownProtobufLibrary();
+    return 0;
+}
--- write-test.cpp
+++ write-test.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <string>
 #include "protobuf/work.pb.h"
-#include "protobuf/addressbook.pb.h"
 
 
 int main(int argc, char* argv[]) {
@@ -10,71 +9,44 @@ int main(int argc, char* argv[]) {
     //auto wm = std::shared_ptr<google::protobuf::Message>(new prototask::WrapperMessage);
     int mode = 1;
     std::string file, datetime;
+
     prototask::FastResponse fr;
     datetime = "31121488";
     fr.set_current_date_time(datetime);
-    wm.set_allocated_fast_response(&fr);
+    //wm.set_allocated_fast_response(&fr);
     file = "proto1.pb";
-    switch (mode) {
-        case 1:{
-            //prototask::FastResponse fr;
-            //datetime = "31121488";
-            //fr.set_current_date_time(datetime);
-            //wm.set_allocated_fast_response(&fr);
-            //file = "proto1.pb";
-            break;
-        };
-        case 2:{
-            prototask::SlowResponse sr;
-            sr.set_connected_client_count(26);
-            wm.set_allocated_slow_response(&sr);
-            wm.clear_fast_response();
-            wm.clear_request_for_fast_response();
-            wm.clear_request_for_slow_response();
-            file = "proto2.pb";
-            break;
-        };
-        case 3:{
-            prototask::RequestForFastResponse rfr;
-            rfr.New();
-            wm.set_allocated_request_for_fast_response(&rfr);
-            wm.clear_fast_response();
-            wm.clear_slow_response();
-            wm.clear_request_for_slow_response();
-            file = "proto3.pb";
-            break;
-        };
-        case 4:{
-            prototask::RequestForSlowResponse rsr;
-            rsr.set_time_in_seconds_to_sleep(25);
-            wm.set_allocated_request_for_slow_response(&rsr);
-            wm.clear_slow_response();
-            wm.clear_fast_response();
-            wm.clear_request_for_fast_response();
-            file = "proto4.pb";
-            break;
-        };
-        default:{
-            prototask::FastResponse fr;
-            fr.set_current_date_time("31121488");
-            wm.set_allocated_fast_response(&fr);
-            prototask::SlowResponse sr;
-            sr.set_connected_client_count(26);
-            wm.set_allocated_slow_response(&sr);
-            prototask::RequestForFastResponse rfr;
-            rfr.New();
-            wm.set_allocated_request_for_fast_response(&rfr);
-            prototask::RequestForSlowResponse rsr;
-            rsr.set_time_in_seconds_to_sleep(25);
-            wm.set_allocated_request_for_slow_response(&rsr);
-            file = "proto_all.pb";
-        };
-    }
+
+    prototask::SlowResponse sr;
+    sr.set_connected_client_count(26);
+    //wm.set_allocated_slow_response(&sr);
+    //file = "proto2.pb";
+
+    prototask::RequestForFastResponse rfr;
+    rfr.New();
+    //wm.set_allocated_request_for_fast_response(&rfr);
+    //file = "proto3.pb";
+
+    prototask::RequestForSlowResponse rsr;
+    rsr.set_time_in_seconds_to_sleep(25);
+    //wm.set_allocated_request_for_slow_response(&rsr);
+    //file = "proto4.pb";
+
+    fr.set_current_date_time("31121488");
+    wm.set_allocated_fast_response(&fr);
+    sr.set_connected_client_count(26);
+    wm.set_allocated_slow_response(&sr);
+    rfr.New();
+    wm.set_allocated_request_for_fast_response(&rfr);
+    rsr.set_time_in_seconds_to_sleep(25);
+    wm.set_allocated_request_for_slow_response(&rsr);
+    file = "proto_all.pb";
+
+
     std::fstream output(file,std::ios::out | std::ios::trunc | std::ios::binary);
-    //if (!wm.SerializeToOstream(&output)){
-    //    std::cerr<<"failed to write\n";
-    //    return -1;
-    //}
+    if (!wm.SerializeToOstream(&output)){
+        std::cerr<<"failed to write\n";
+        return -1;
+    }
 
     std::cout<<wm.SerializeAsString()<<std::endl;
     output.close();
